6ba62eb8523ebc3dc5b48367b38c3785fa54e503
Rogério Carvalho <rogerio.carvalho@unifesspa.edu.br>
2021-05-25 23:23:04 -0300

    aula 3

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from "react";
-import App from "./App";
-import {render, screen} from "@testing-library/react";
+import App, {calcularNovoSaldo} from "./App";
+import {fireEvent, render, screen} from "@testing-library/react";
 
 describe('Componente principal', () => {
     describe('Quando eu abro o app o banco', () => {
@@ -20,5 +20,34 @@ describe('Componente principal', () => {
 
             expect(screen.getByText('Realizar operação')).toBeInTheDocument();
         })
+    })
+
+    describe('Quando eu realizo uma transação', () => {
+        it('que é um saque, ovlor vai diminuir', () => {
+            const valores = {
+                transacao: 'saque',
+                valor: 50
+            };
+            const novoSaldo = calcularNovoSaldo(valores, 150)
+            expect(novoSaldo).toBe(100);
+        });
+
+        it('que é um saque, a transação deve ser realizada', () => {
+            render(<App/>)
+
+            const saldo = screen.getByText('R$ 1000');
+
+            const transacao = screen.getByLabelText("Saque");
+            const valor = screen.getByTestId('valor');
+            const botaoTransacao = screen.getByText('Realizar operação')
+
+            expect(saldo.textContent).toBe('R$ 1000')
+
+            fireEvent.click(transacao, {target: {value: 'saque'}});
+            fireEvent.change(valor, {target: {value: 10}});
+            fireEvent.click(botaoTransacao);
+            expect(saldo.textContent).toBe('R$ 990');
+
+        });
     })
 })
